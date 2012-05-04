e0b3733dff58e30c980a74376515349df09ad1f4
Alex Kaos <gozulin@gmail.com>
2012-05-04 16:10:35 +0000

    added  base_enemy and base_weapon classes

---
+++ base_enemy.h
@@ -0,0 +1,14 @@
+//
+//  base_enemy.h
+//  DeathHold
+//
+//  Created by Al Kaos on 5/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef DeathHold_base_enemy_h
+#define DeathHold_base_enemy_h
+
+
+
+#endif
---
+++ base_weapon.h
@@ -0,0 +1,14 @@
+//
+//  base_weapon.h
+//  DeathHold
+//
+//  Created by Al Kaos on 5/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef DeathHold_base_weapon_h
+#define DeathHold_base_weapon_h
+
+
+
+#endif
