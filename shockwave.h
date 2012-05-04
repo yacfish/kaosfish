a16a89f5949a994b4934773c4866d16848051bf7
Alex Kaos <gozulin@gmail.com>
2012-05-04 17:01:27 +0000

    added shockwave class

---
+++ shockwave.h
@@ -0,0 +1,12 @@
+//
+//  shockwave.h
+//  DeathHold
+//
+//  Created by Al Kaos on 5/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef DeathHold_shockwave_h
+#define DeathHold_shockwave_h
+
+#endif
